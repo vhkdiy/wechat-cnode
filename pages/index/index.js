3e52d989282b448483c062cc00344c6998ca4359
vhkdiy <404924175@qq.com>
2017-02-04 18:19:05 +0800

    update switch-tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,25 @@ const { getDateDiff } = require('../../utils/util.js');
 Page({
   data: {
     title: '首页列表',
-    topics: []
+    topics: [],
+    navList: [ {tab: 'all', name: '全部'}, {tab: 'good', name: '精华'}, {tab: 'share', name: '分享'}, {tab: 'ask', name: '问答'}, {tab: 'job', name: '招聘'} ],
+    activeIndex: 0,
   },
   onLoad() {
     console.log('onload by topics');
     this.fetchData();
+  },
+
+  switchTab(e) {
+    const tab = e.currentTarget.id;
+    const index = e.currentTarget.dataset.index;
+
+    this.fetchData({tab});
+    this.setData({
+      activeIndex: index
+    });
+
+    
   },
 
   fetchData(data) {
@@ -20,15 +34,31 @@ Page({
         'Accept': 'application/json'
       },
       success: (res) => {
+        console.log(res.data.data)
         res.data.data.map(item => {
           item.last_reply_at = getDateDiff(item.last_reply_at);
           item.create_at = getDateDiff(item.create_at);
           return item;
-        })
+        });
         this.setData({
           topics: res.data.data
-        })
+        });
+
+        wx.showToast({
+          title: '加载中',
+          icon: 'loading',
+          duration: 10000
+        });
+        
+        setTimeout(function(){
+          wx.hideToast()
+        },1000)
+        
       }
     })
+  },
+
+  contentPage(e) {
+    
   }
 })
